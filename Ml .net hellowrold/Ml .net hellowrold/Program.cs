9819e57cc44932414ab3a680b1a0b9f06989893a
oskar k <s19732@pjwstk.edu.pl>
2020-05-29 15:50:22 +0200

    first commit

---
+++ Ml .net hellowrold/Ml .net hellowrold/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Ml__net_hellowroldML.Model;
+
+namespace Ml_.net_hellowrold
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = new ModelInput();
+            input.SentimentText = "That is rude";
+
+
+
+            ModelOutput result = ConsumeModel.Predict(input);
+
+            Console.WriteLine($"Text: {input.SentimentText}\n is is Toxic: {result.Prediction}");
+        }
+    }
+}
